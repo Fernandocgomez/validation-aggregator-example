371191fa79c2c068e2e469d458dc78a9966641fb
Fernandocgomez <fernandocgomez@live.com>
2024-10-25 21:51:09 -0500

    remove files and config prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	imports: [],
+	template: 'hello',
 })
-export class AppComponent {
-  title = 'validation-aggregator-example';
-}
+export class AppComponent {}
